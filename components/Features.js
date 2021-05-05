77c2f538644e3ede83e5c0371cc59c46cc52953d
Murad Cholukov <muradusa@gmail.com>
2021-05-04 17:50:35 -0700

    adding content to features

--- components/Features.js
+++ components/Features.js
@@ -67,14 +67,14 @@ export default function Example() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="lg:text-center">
           <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
-            Transactions
+            Cool Tech
           </h2>
           <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            A better way to send money
+            A better way to build apps
           </p>
           <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-            Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam
-            voluptatum cupiditate veritatis in accusamus quisquam.
+            Here are some cool technologies i have been playing with to make my
+            apps more performant and nice looking.
           </p>
         </div>
 
