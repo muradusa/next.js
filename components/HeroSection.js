847fd364fc0e9867b8d08745fc21c90034257243
Murad Cholukov <muradusa@gmail.com>
2021-05-06 06:08:05 -0700

    adding content to features

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -141,7 +141,7 @@ export default function Example() {
                         href="#"
                         className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
                       >
-                        Get started
+                        Resume
                       </a>
                     </div>
                     <div className="mt-3 sm:mt-0 sm:ml-3">
@@ -149,7 +149,7 @@ export default function Example() {
                         href="#"
                         className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
                       >
-                        Live demo
+                        Contact
                       </a>
                     </div>
                   </div>
